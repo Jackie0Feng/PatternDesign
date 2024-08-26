38af144f430ab465b584080bff1b01969cab2300
jackie feng <2571617419@qq.com>
2024-08-26 18:27:44 +0800

    原始状态模式实现

--- StrategyPattern/main.cs
+++ StrategyPattern/main.cs
@@ -20,29 +20,29 @@ using MementoPattern;
 
 public interface IPattern
 {
-    void Main();
+	void Main();
 }
 
 
 internal class main
 {
-    public static void Main()
-    {
-        //IPattern pattern = new StrategyPatternMain();
-        //IPattern pattern = new ObserverPatternMain();
-        //IPattern pattern = new DecoratorPatternMain();
-        //IPattern pattern = new FactoryPatternMain();
-        //IPattern pattern = new AbstractFactoryPatternMain();
-        //IPattern pattern = new SingletonPatternMain();
-        //IPattern pattern = new CommandPatternMain();
-        //IPattern pattern = new AdapterPatternMain();
-        //IPattern pattern = new FacadePatternMain();
-        //IPattern pattern = new FacadePatternMain();
-        //IPattern pattern = new TemplatePatternMain();
-        //IPattern pattern = new IteratorPatternMain();
-        //IPattern pattern = new CompositePatternMain();
-        //IPattern pattern = new StatePatternMain();
-        IPattern pattern = new MementoPatternMain();
-        pattern.Main();
-    }
+	public static void Main()
+	{
+		//IPattern pattern = new StrategyPatternMain();
+		//IPattern pattern = new ObserverPatternMain();
+		IPattern pattern = new DecoratorPatternMain();
+		//IPattern pattern = new FactoryPatternMain();
+		//IPattern pattern = new AbstractFactoryPatternMain();
+		//IPattern pattern = new SingletonPatternMain();
+		//IPattern pattern = new CommandPatternMain();
+		//IPattern pattern = new AdapterPatternMain();
+		//IPattern pattern = new FacadePatternMain();
+		//IPattern pattern = new FacadePatternMain();
+		//IPattern pattern = new TemplatePatternMain();
+		//IPattern pattern = new IteratorPatternMain();
+		//IPattern pattern = new CompositePatternMain();
+		//IPattern pattern = new StatePatternMain();
+		//IPattern pattern = new MementoPatternMain();
+		pattern.Main();
+	}
 }
