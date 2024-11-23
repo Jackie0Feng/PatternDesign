d2192206b5b25f1b9576affe11e2c050f7680ecc
jackie feng <2571617419@qq.com>
2024-11-23 08:10:20 +0800

    升级.net8.0项目，引入单元测试

---
+++ StrategyPattern/Refactor/TestClass.cs
@@ -0,0 +1,27 @@
+﻿/*************************************************************************************
+ *
+ * 文 件 名:   TestClass
+ * 描    述: TestClass
+ * 
+ * 版    本：  V1.0
+ * 创 建 者：  jackie 
+ * 创建时间：  2024/11/21 11:59:55
+*************************************************************************************/
+
+using System;
+using System.Collections.Generic;
+
+namespace PatternDesign
+{
+	public class TestClass
+	{
+		// Public field
+		public void HelloWorld()
+		{
+			Console.WriteLine("Hello World!");
+		}
+
+		// Private field
+
+	}
+}
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using PatternDesign;
+
+namespace TestProject1
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			string Expected = "Hello World!";
+
+			using (var sw = new StringWriter())
+			{
+				Console.SetOut(sw);
+
+				TestClass testClass = new TestClass();
+				testClass.HelloWorld();
+
+				var result = sw.ToString().Trim();
+				Assert.AreEqual(Expected, result);
+			}
+		}
+	}
+}
